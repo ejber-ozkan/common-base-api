4e8c17c069efa8a46deb89b272ff5bfddf71e17e
Ejber Ozkan <ejber@ozkan.co.uk>
2019-10-31 13:51:36 +0000

    logging to stdout is probably better

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # common-base-api
+
+docker build -t common-base-api .
+docker run -p 8080:8080 common-base-api:lates
--- utils/logger.go
+++ utils/logger.go
@@ -7,11 +7,13 @@ import (
 	"github.com/gorilla/handlers"
 )
 
-//APILoggingHandler - an attempt to log something
+//APILoggingHandler - an attempt to log something to STDOUT
 func APILoggingHandler(h http.Handler) http.Handler {
-	logFile, err := os.OpenFile("server.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		panic(err)
-	}
-	return handlers.LoggingHandler(logFile, h)
+
+	// to log to file (Commented out for now .. maybe a 'dev' only switch)
+	//logFile, err := os.OpenFile("server.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	//if err != nil {
+	//	panic(err)
+	//}
+	return handlers.LoggingHandler(os.Stdout, h)
 }
