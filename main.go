2799162ac301ce9cd834bf9d59363b80c7b1b826
Ejber Ozkan <ejber.ozkan@bbc.co.uk>
2020-09-24 18:22:26 +0100

    bug?

--- main.go
+++ main.go
@@ -18,21 +18,16 @@ import (
 
 	opentracing "github.com/opentracing/opentracing-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
-	jaegerlog "github.com/uber/jaeger-client-go/log"
-	"github.com/uber/jaeger-lib/metrics"
 )
 
 func main() {
 
 	cfg, err := jaegercfg.FromEnv()
 
-	jLogger := jaegerlog.StdLogger
-	jMetricsFactory := metrics.NullFactory
+	//jLogger := jaegerlog.StdLogger
+	//jMetricsFactory := metrics.NullFactory
 	// Initialize tracer with a logger and a metrics factory
-	tracer, closer, err := cfg.NewTracer(
-		jaegercfg.Logger(jLogger),
-		jaegercfg.Metrics(jMetricsFactory),
-	)
+	tracer, closer, err := cfg.NewTracer()
 
 	if err != nil {
 		fmt.Println(fmt.Errorf("Error: %v", err))
